df5fd71df69e342d08f688b599cf3b51ae9d67de
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-22 10:50:56 -0300

    Atualização das pastas

---
+++ códigos prontos/exemplos.py
@@ -0,0 +1,40 @@
+"""
+    Numpy Arrays
+To define an array in Python, you could use the np.array function to convert a list.
+x = np.array([[1, 4, 3], [9, 2, 7]])
+
+Find the rows, columns and the total size for array x.
+x.shape
+x.size
+
+Create an array z from 1 to 2000 with an increment 1.
+z = np.arange(1, 2000, 1)
+
+Use linspace to generate an array starting at 3, ending at 9, and containing 10 elements.
+np.linspace(3, 9, 10)
+
+Generate a 3 by 5 array with all the as 0.
+np.zeros((3, 5))
+
+Generate a 5 by 3 array with all the element as 1.
+np.ones((5, 3))
+
+Compute the transpose of array b.
+b.T
+
+    Functions
+Here we will introduce the most common way to define a function which can be specified using the keyword def, as showing in the following:
+    def function_name(argument_1, argument_2, ...):
+        '''
+        Descriptive String
+        '''
+        # comments about the statements
+        function_statements 
+        
+        return output_parameters (optional)
+
+
+
+
+
+"""
---
+++ lista2/questao1c.py
@@ -0,0 +1,39 @@
+import math;
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+def bissection(f, lowerLimit, higherLimit, tol, iterationNumber=100):
+    a = lowerLimit;
+    b = higherLimit;
+    err = b - a;
+    i = 0;
+
+    while (err > tol):
+        x = (a + b)/2;
+        i = i + 1;
+        
+        if i >= iterationNumber:
+            break;
+
+        if(f(x) * f(a) > 0):
+            a = x;
+
+        else:
+            b = x;
+        err = b - a;
+
+    return [x, err, i]
+    
+
+# função em questão que quero achar a raiz
+def f(x):
+    A = 27*x**3 + 27/2*x**4 + 9*x**5/4 + (x**6)/8
+    B = 3+x
+    Q = 20
+    g = 9.81
+    return (B)*Q**2 - g*(A)
+
+root_f, error, iteration = bissection(f, 0.1, 2.5, 0.01,10)
+
+# str(number) só transforma um número em string
+print('A raiz de f vale ' + str(root_f) + ', com erro ' + str(error) + 'e com ' + str(iteration) + ' iteracoes.')
