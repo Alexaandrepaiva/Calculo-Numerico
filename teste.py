53edb86ab4df9c65c5b539e7eedfb9a78fdc512b
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 16:37:01 -0300

    Bissection Final

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
-import zeros
-import numpy as np
+import zeros;
+import numpy as np;
+import matplotlib.pyplot as plt;
 
 x = np.arange(0, 3, 0.01)
 A = 27*x**3 + 27/2*x**4 + 9*x**5/4 + (x**6)/8
@@ -7,8 +8,3 @@ B = 3+x
 y = B*20**2 - 9.81*A
 
 
-
-f = lambda x : x**2
-
-
-zeros.graphically(x, f(x))
--- zeros.py
+++ zeros.py
@@ -1,10 +1,9 @@
 # Feito por Alexandre Paiva
 # Biblioteca criada para disciplina de Cálculo Numérico
-import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt;
+import numpy as np;
 
-def graphically(x, y):
-    plt.title('Questão 1a')
+def graph(x, y):
     plt.xlabel('X-Axis')
     plt.ylabel('Y-Axis')
     ax = plt.gca()
@@ -16,29 +15,30 @@ def graphically(x, y):
     plt.show()
 
 
-def bissection(f, lowerLimit, higherLimit, tol, iterationNumber=100):
-    err = higherLimit - lowerLimit
+def bissection(f, lowerLimit = -1e5, higherLimit = 1e5, tolerance = 1e-5, iterationNumber = 1e5):
     i = 0
+    
+    if f(lowerLimit)*f(higherLimit) > 0:
+        return "Interval error"
 
-    while ((err > tol) and (i >= iterationNumber)):
+    while (( abs(lowerLimit - higherLimit) > tolerance) and (i >= iterationNumber)):
         x = (lowerLimit + higherLimit)/2
         i = i + 1
-
-        if(f(x) * f(lowerLimit) > 0):
+        if f(x) == 0:
+            break
+        elif(f(x) * f(lowerLimit) > 0):
             lowerLimit = x
-
         else:
             higherLimit = x
-        err = np.abs(f(x))
 
-    return [x, err, i]
+    return [x, abs(lowerLimit - higherLimit), i]
 
 
-def pontoFixo(f, phi, initialValue, tol, iterationNumber):
+def pontoFixo(f, phi, initialValue, tolerance, iterationNumber):
     x_k = [initialValue]
     i = 0
 
-    while(abs(f(x_k[-1])) > tol and i < iterationNumber):
+    while(abs(f(x_k[-1])) > tolerance and i < iterationNumber):
         x_next = phi(x_k[-1])
         x_k.append(x_next)
         i += 1
@@ -48,13 +48,13 @@ def pontoFixo(f, phi, initialValue, tol, iterationNumber):
     return [x_k, err, i]
 
 
-def falsePositive(f, lowerLimit, higherLimit, tol, iterationNumber=100):
+def falsePositive(f, lowerLimit, higherLimit, tolerance, iterationNumber=100):
     a = lowerLimit
     b = higherLimit
     err = b - a
     i = 0
 
-    while (err > tol):
+    while (err > tolerance):
         # calcula x como a média ponderada
         # de resto, idêntico à bisseção
         x = (a * abs(f(b)) + b * abs(f(a)))/(abs(f(a)) + abs(f(b)))
@@ -73,14 +73,14 @@ def falsePositive(f, lowerLimit, higherLimit, tol, iterationNumber=100):
     return [x, err, i]
 
 
-def newton_raphson(f, dfdx, initialValue, tol, iterationNumber):
+def newton_raphson(f, dfdx, initialValue, tolerance, iterationNumber):
 
     i = 0  # vai contar as iterações
 
     x_k = [initialValue]  # array com os valores testados de x
     y_k = [f(initialValue)]  # array com os correspondentes f(x)
 
-    while(abs(f(x_k[-1])) > tol or i > iterationNumber):
+    while(abs(f(x_k[-1])) > tolerance or i > iterationNumber):
 
         # método para calcular o próximo valor de x
         # é onde a reta tangente em initialValue cruza as abcissas
