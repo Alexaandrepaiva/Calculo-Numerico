22f3b119871a998f773da9c7f19a01e3846d2a11
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-28 12:45:52 -0300

    Fixed Point

--- zeros.py
+++ zeros.py
@@ -58,11 +58,12 @@ def falsePositive(f, lowerLimit= -1e5, higherLimit = 1e5, tolerance = 1e-5, iter
 def fixedPoint(f, phi, initialValue = 0, tolerance = 1e-5, iterationNumber = 1e5):
     i = 0
     x = phi(initialValue)
-    while(abs( initialValue - x) > tolerance and i < iterationNumber):
+    while abs(initialValue - x) > tolerance and i < iterationNumber:
         i += 1
-        initialValue, x = x, phi(x)
+        initialValue = x
+        x = phi(x)
     return [x, i]
-#raiz, erro, iteracoes = zeros.fixedPoint(f, phi, -1, 0.001, 10)
+#raiz, iteracoes = zeros.fixedPoint(f, phi, -1, 0.001, 10)
 #print('A raiz de f pelo método da iteração de ponto fixo é {:.7f} e com {} iteracoes.'.format(raiz, iteracoes))
 
 
