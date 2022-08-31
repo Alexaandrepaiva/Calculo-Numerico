2e5a2e263b877cfcb8d54422dab7097c3fd2e2bf
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-31 14:58:45 -0300

    Gauss Elimination

--- linearSystem.py
+++ linearSystem.py
@@ -48,6 +48,45 @@ def retroactiveReplacement(matrixU, b_s):
         x_s[i] = (b_s[i] - matrixU[i, i+1:]@x_s[i+1:])/matrixU[i, i]
     return x_s
 
+#Eliminação de Gauss
+def gauss(table):
+    table_np = np.array(table)
+    n_lines = len(table_np)
+    n_colum = len(table_np[0])
+
+    if n_lines+1!=n_colum:
+        exit("We can't solve this... Please make sure this is right.")
+
+    for i in range(n_lines):
+        if table_np[i][i] !=0:
+            pivot = table_np[i][i]
+        else:
+            for j in range(i+1,n_lines):
+                if table_np[j][i]:
+                    ans = list(table_np[i])
+                    table_np[i] = -table_np[j]
+                    table_np[j] = np.array(ans)
+                    pivot = table_np[i][i]
+                    break
+
+        for j in range(i+1,n_lines):
+            if not table_np[j][i]:
+                continue
+            else:
+                table_np[j] = table_np[j]*pivot - table_np[j][i]*table_np[i]
+    print(table_np)
+    next_roots = list() #implementação da parte que pegar as raizes.
+    for i in range(n_lines):
+        soma = 0
+        root = 0
+        for j in range(i):
+            soma += next_roots[j]*table_np[n_lines-1-i][n_colum-2-j]
+        if table_np[n_lines-1-i][n_lines-1-i]:
+            root =  (table_np[n_lines-1-i][n_colum-1] - soma)/table_np[n_lines-1-i][n_lines-1-i]
+        next_roots.append(root)
+    next_roots.reverse()
+    return next_roots
+
 # Eliminacao de Gauss com pivotamento.- Resolve A*x = B. 
 # Recebe A, B, escalona-os e depois resolve usando a funcao retroactiveReplacement() e retorna x
 def gaussEliminationPivot(inA, inb):
